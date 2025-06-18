4253efb0bb1d2bc22b90143f0ccb3185bb8486cb
zaidyargatti <9928zaid@gamil.com>
2025-06-18 14:18:19 +0530

    trying to fix

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
